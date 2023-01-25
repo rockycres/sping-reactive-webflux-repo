f06d89a3ba8cf250bd1e96e13942ceb2370c93ce
rockycres <98057055+rockycres@users.noreply.github.com>
2023-01-25 17:13:17 +1100

    initial commit

---
+++ sping-reactive-webflux/src/main/java/com/learn/reactive/SpingReactiveWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.learn.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingReactiveWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingReactiveWebfluxApplication.class, args);
+    }
+
+}
---
+++ sping-reactive-webflux/src/test/java/com/learn/reactive/SpingReactiveWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingReactiveWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
